f662caabc4bb00c46d7b3fbf6873f08c86011b46
mohitgoyat525 <montyits71@gmail.com>
2025-01-20 14:59:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import FilterParctise from './components/FilterParctise';
+
+function App() {
+  return (
+    <>
+      <FilterParctise />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FilterParctise.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+const FilterPractice = () => {
+  const [name, setName] = useState(""); 
+  const [lastName, setLastName] = useState("");
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [email, setEmail] = useState("");
+
+  const submit = (event) => {
+    event.preventDefault();
+    const newData = [...data];
+    newData.push({ name, lastName, email });
+    setData(newData);
+    setName("");
+    setLastName("");
+    setEmail("");
+  };
+
+const filteredData = data.filter((item) => item.name.includes(search)||item.lastName.includes(search)||item.email.includes(search));
+    return (
+      <div className="min-h-screen bg-gray-50 flex flex-col items-center py-10 justify-center">
+        <form
+          className="bg-white shadow-lg rounded-lg p-8 max-w-[600px] w-full mx-auto"
+          onSubmit={submit}
+        >
+          <div className="flex flex-col space-y-4">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="border border-gray-300 rounded-lg p-4 outline-none"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="border border-gray-300 rounded-lg p-4 outline-none"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="border border-gray-300 rounded-lg p-4 outline-none"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="search"
+              placeholder="Search"
+              className="border border-gray-300 rounded-lg p-4 outline-none"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-blue-600 text-white py-3 rounded-lg font-medium hover:bg-purple-700 ease-linear duration-300 transition-all"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-8 w-full max-w-[600px] mx-auto">
+          <h2 className="text-xl text-gray-800 font-semibold mb-4">
+            Search Results
+          </h2>
+          <div className="my-4">
+            {search && filteredData.length === 0 ? (
+              <p className="text-center text-gray-500">No users found</p>
+                ):(
+              filteredData.map((item, i) => (
+                <div key={i} className="bg-white shadow-md rounded-lg p-6">
+                  <p className="text-lg font-semibold text-gray-800">
+                    First Name:- {item.name}
+                  </p>
+                  <p className="text-lg font-semibold text-gray-800">
+                    Last Name:- {item.lastName}
+                  </p>
+                  <p className="text-gray-600">Email:- {item.email}</p>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default FilterPractice;
